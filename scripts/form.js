3bff0c67976f92745cb14b847c11f4888915935c
Anna <snusha.spb@gmail.com>
2023-12-04 13:32:49 +0300

    fix: lab5

--- scripts/form.js
+++ scripts/form.js
@@ -40,21 +40,45 @@ document.getElementById("tableForm").addEventListener("submit", function (event)
     localStorage.setItem("service", service);
     localStorage.setItem("dayOfWeek", dayOfWeek);
     localStorage.setItem("time", time);
+    console.log(localStorage.getItem("service"));
+    console.log(localStorage.getItem("dayOfWeek"));
+    console.log(localStorage.getItem("time"));
 });
 
-// Загрузка сохраненных параметров из LocalStorage
-var savedService = localStorage.getItem("service");
-var savedDayOfWeek = localStorage.getItem("dayOfWeek");
-var savedTime = localStorage.getItem("time");
 
-if (savedService ) {
-    document.getElementById("service").value = savedService;
-}
+window.addEventListener('load', () =>{
+    var savedService = localStorage.getItem("service");
+    var savedDayOfWeek = localStorage.getItem("dayOfWeek");
+    var savedTime = localStorage.getItem("time");
+    var select = document.getElementById("dayOfWeek");
+    console.log(localStorage.getItem("service"));
+    console.log(localStorage.getItem("dayOfWeek"));
+    console.log(localStorage.getItem("time"));
+    console.log(select);
+    if (savedService && savedDayOfWeek && savedTime) {
+        var tableContent = "<table>";
 
-if (savedDayOfWeek) {
-    document.getElementById("dayOfWeek").value = savedDayOfWeek;
-}
+        for (var i = -1; i < 24; i++) {
+            for (var j = -1; j < select.length; j++) {
+                if (i === -1 && j === -1) {
+                    tableContent += "<th></th>";
+                } else if (j === -1) {
+                    tableContent += "<th>" + i + ":00</th>";
+                } else if (i === -1) {
+                    tableContent += "<th>" + select[j].value + "</th>";
+                } else if ((select[j].value === savedDayOfWeek) && (i === Number(savedTime))){
+                    console.log(select[j].value === savedDayOfWeek);
+                    console.log(i === savedTime);
+                    tableContent += "<td>"+savedService+"</td>";
+                } else {
+                    tableContent += "<td></td>";
+                }
+            }
+            tableContent += "</tr>";
+        }
 
-if (savedTime) {
-    document.getElementById("time").value = savedTime;
-}
+        tableContent += "</table>";
+
+        document.getElementById("tableContainer").innerHTML = tableContent;
+    }
+});
